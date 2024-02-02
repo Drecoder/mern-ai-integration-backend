3822c77da121e5e98bbf8ae7544d91463948883e
Andres Arias <ariasa83@gmail.com>
2024-02-02 15:03:45 -0600

    server & models(Users.js)

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+//Schema
+const userSchema = new mongoose.Schema({
+    userSchema: {   
+        type: String,
+        required:true,
+    },
+    emai: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    trialActive: {
+        type: Boolean,
+        required: true,     
+    },
+    trialExpires: {
+        type: Date,
+    },
+    subscription: {
+        type: String,
+        enum:['Trial', 'Free', 'Basic', 'Premium'],
+    },
+    apiRequestCount:{
+        type: Number,
+        defaut: 0,
+    },  
+    monthlyuRequestCount:{
+        type: Number,
+        defaut: 0,
+    },
+    nextBillingDate: Date,
+    payments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Payment',
+        },
+    ],
+    history: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'History',
+        },
+    ],
+});
