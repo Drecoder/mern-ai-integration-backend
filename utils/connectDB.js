21f0d07a4dbc3744d21bdc4c7e2d8327f509c659
Andres Arias <ariasa83@gmail.com>
2024-02-03 14:27:11 -0600

    connedDB

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 // PASSWORD = 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect("mongodb+srv://andresariasrecruiter:5zi9ML99L72FbMoM@mern-ai-integration.tzolwsx.mongodb.net/?retryWrites=true&w=majority");
+        const conn = await mongoose.connect();
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 } catch (err) {
